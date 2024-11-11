cb8a251707d37c47be4acfc051704dca0d34bcd5
ruglawy <git@github.com>
2024-11-11 14:57:37 +0200

    Volunteer now extends Person

--- src/main/java/com/blooddonation/system/models/people/Volunteer.java
+++ src/main/java/com/blooddonation/system/models/people/Volunteer.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Volunteer implements IVolunteer {
+public class Volunteer extends Person implements IVolunteer {
     private boolean isAvailable;
     private List<String> skills;
 
