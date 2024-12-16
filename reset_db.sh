ab04fe7b8e85a4a37969740cca01aae16cd00e6e
ZiadAmerr <xeiad.amerr@yahoo.com>
2024-12-17 00:53:45 +0200

    Some reorganization

--- reset_db.sh
+++ reset_db.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 DB_USER="root" # Replace with your MySQL username if different
-DB_PASS="1234"
-DB_HOST="127.0.0.1"
+DB_HOST="localhost"
 DB_PORT="3307"
 SQL_FILE_PATH="/Applications/XAMPP/xamppfiles/htdocs/sql_reset.sql"
 MYSQL_PATH="/Applications/XAMPP/xamppfiles/bin/mysql"
@@ -12,7 +11,7 @@ if [ ! -f "$SQL_FILE_PATH" ]; then
     exit 1
 fi
 
-COMMAND="$MYSQL_PATH --user=\"$DB_USER\" --password=\"$DB_PASS\" --host=\"$DB_HOST\" --port=\"$DB_PORT\" < \"$SQL_FILE_PATH\""
+COMMAND="$MYSQL_PATH --user=\"$DB_USER\" --host=\"$DB_HOST\" --port=\"$DB_PORT\" < \"$SQL_FILE_PATH\""
 echo "Executing: $COMMAND"
 
 eval $COMMAND
