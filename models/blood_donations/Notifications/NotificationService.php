0e1bdad9251874295e3ddfce3ceef865a2db57ab
ZiadAmerr <xeiad.amerr@yahoo.com>
2025-01-17 13:27:50 +0200

    Change the signature of Notifications

--- models/blood_donations/Notifications/NotificationService.php
+++ models/blood_donations/Notifications/NotificationService.php
@@ -2,7 +2,7 @@
 
 interface NotificationService {
     // notify_user should take a user JSON object and a message string
-    public function notify(string $number, string $message);
+    public function notify($data);
 }
 
 ?>
